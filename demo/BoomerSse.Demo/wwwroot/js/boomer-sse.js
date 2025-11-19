8b9636fa5779f7f6b94c25e28b08aaab0a02bed2
Rob Johnson <rjohnson@appliedsystems.com>
2025-11-19 20:53:45 +0000

    Starting to use Razor pages and starting to develop the sse js

---
+++ demo/BoomerSse.Demo/wwwroot/js/boomer-sse.js
@@ -0,0 +1,56 @@
+﻿'use strict';
+
+const getEventData = (el, propName) => {
+    const dataSetValue = el.dataset[propName];
+
+    if (!dataSetValue) {
+        console.warn(`No dataset value for ${propName}`);
+        return undefined;
+    }
+
+    const splitValues = dataSetValue.split('|');
+
+    if (!splitValues || splitValues.length < 1 || !splitValues[0] || splitValues[0].length < 1) {
+        console.warn(`Dataset value is invalid ${dataSetValue}`);
+        return undefined;
+    }
+
+    const data = splitValues.length > 1
+        ? splitValues[1]
+        : null;
+
+    return {
+        event: splitValues[0],
+        data
+    };
+};
+
+const bindOnLoad = (el) => {
+    const eventData = getEventData(el, "bsseOnload");
+    console.log(eventData);
+};
+
+const bindOnClick = (el) => {
+    const eventData = getEventData(el, "bsseOnclick");
+    console.log(eventData);
+};
+
+const bindOnSubmit = (el) => {
+    const eventData = getEventData(el, "bsseOnsubmit");
+    console.log(eventData);
+};
+
+const init = () => {
+
+    const onLoadElements = document.querySelectorAll("[data-bsse-onload]");
+    const onClickElements = document.querySelectorAll("[data-bsse-onclick]");
+    const onSubmitElements = document.querySelectorAll("form[data-bsse-onsubmit]");
+
+    onLoadElements.forEach(bindOnLoad);
+    onClickElements.forEach(bindOnClick);
+    onSubmitElements.forEach(bindOnSubmit);
+};
+
+window.addEventListener("DOMContentLoaded", () => {
+    init();
+});
---
+++ src/BoomerSse/Bootstrapping.cs
@@ -0,0 +1,6 @@
+﻿namespace BoomerSse;
+
+public static class Bootstrapping
+{
+    
+}
