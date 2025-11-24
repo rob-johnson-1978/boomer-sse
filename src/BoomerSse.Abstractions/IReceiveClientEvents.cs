d55682a1505e73a5742632ca6330fbeedfc75f53
Rob Johnson <rjohnson@appliedsystems.com>
2025-11-24 08:13:12 +0000

    More yieldyness

--- src/BoomerSse.Abstractions/IReceiveClientEvents.cs
+++ src/BoomerSse.Abstractions/IReceiveClientEvents.cs
@@ -3,4 +3,6 @@
 public interface IReceiveClientEvents
 {
     Task Receive(Guid sessionId, ClientEventBody clientEventBody, CancellationToken cancellationToken);
+
+    Task SubscribeToReceivedEvents(Guid sessionId, CancellationToken cancellationToken);
 }
--- src/BoomerSse/RequestHandlers.cs
+++ src/BoomerSse/RequestHandlers.cs
@@ -1,6 +1,8 @@
 ﻿using BoomerSse.Abstractions;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Net.ServerSentEvents;
+using System.Threading.Channels;
 
 namespace BoomerSse;
 
@@ -17,16 +19,47 @@ internal class RequestHandlers
         return Results.Accepted();
     }
 
-    internal static IResult Sub(
-        [FromServices] ClientStreamSubscriber clientStreamSubscriber,
-        [FromServices] ServerStreamPublisher serverStreamPublisher,
+    internal static async Task<IResult> Sub(
+        [FromServices] IReceiveClientEvents clientEventReceiver,
         [FromQuery(Name = "session_id")] Guid sessionId,
         CancellationToken cancellationToken)
     {
-        clientStreamSubscriber.StartSubscribing(sessionId, cancellationToken);
+        // todo:
+        // 1. setup a client event handler
+        // 2. subscribe to the client event and forward to the handler
+        // 3. onHandled, yield the server events from the yielder
+
+        var yieldChannel = Channel.CreateUnbounded<SseItem<ServerEventBody>>();
 
         return TypedResults.ServerSentEvents(
-            serverStreamPublisher.StartPublishing(sessionId, cancellationToken)
+            Yield()
         );
+
+        // -------------------------------- //
+
+        async IAsyncEnumerable<SseItem<ServerEventBody>> Yield()
+        {
+            try
+            {
+                while (!cancellationToken.IsCancellationRequested)
+                {
+                    if (!await yieldChannel.Reader.WaitToReadAsync(cancellationToken))
+                    {
+                        continue;
+                    }
+
+                    while (yieldChannel.Reader.TryRead(out var sseItem))
+                    {
+                        yield return sseItem;
+                    }
+                }
+            }
+            finally
+            {
+                yieldChannel.Writer.Complete();
+
+                // todo: dispose of any other resources associated with the sessionId (from strategies)
+            }
+        }
     }
 }
